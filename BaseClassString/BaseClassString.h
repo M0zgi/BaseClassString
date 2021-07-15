39e35d582a5ee40ca80bfbd4be266f725c7da89d
Aros_zi34@student.itstep.org <Aros_zi34@student.itstep.org>
2021-07-15 17:32:08 +0300

    Добавьте файлы проекта.

---
+++ BaseClassString/BaseClassString.h
@@ -0,0 +1,177 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+struct numpunct
+	: std::numpunct<char>
+{
+	std::string do_truename() const { return "Да"; }
+	std::string do_falsename() const { return "Нет"; }
+};
+
+class MyString
+{
+public:
+	
+	// 1). конструктор без параметров;
+	MyString() {str = nullptr; }
+
+	// 2). конструктор, принимающий в качестве параметра C-строку (заканчивается нулевым байтом);
+	MyString(const char* str);
+
+	// 3) конструктор копирования;
+	MyString(const MyString& obj);
+
+	// 4) оператор присваивания;
+	MyString& operator =(const MyString& obj);
+
+	// 5). получение длины строки;
+	int countLenght();
+
+	// 6) очистка строки (сделать строку пустой);
+	void clearString();
+
+	//void print() { cout << str; }
+
+	
+	// 7). деструктор
+	~MyString();
+
+	//8) конкатенация строк(перегрузить операторы + и += );
+	MyString operator+(const MyString& obj);
+
+	MyString &operator+=(const MyString& obj);
+
+	//9) проверка на равенство(==) и на неравенство (!=).
+
+	bool operator==(const MyString& obj);
+
+	bool operator!=(const MyString& obj);
+
+	//перегузка оператора вывода
+	friend ostream& operator << (ostream& out, const MyString& obj);
+
+private:
+	char* str;
+
+};
+
+inline MyString::MyString(const char* str)
+{
+	int lenght = strlen(str);
+	this->str = new char [lenght + 1];
+	strcpy(this->str, str);
+}
+
+inline MyString::MyString(const MyString& obj)
+{
+	int lenght = strlen(obj.str);
+	this->str = new char[lenght + 1];
+	strcpy(this->str, obj.str);
+}
+
+inline MyString& MyString::operator=(const MyString& obj)
+{
+	
+	if (this->str != nullptr)
+	{
+		delete[]this->str;
+	}
+	
+	int lenght = strlen(obj.str);    
+	this->str = new char[lenght + 1];
+	strcpy(this->str, obj.str);
+
+	return *this;
+}
+
+inline int MyString::countLenght()
+{
+	return strlen(this->str);
+}
+
+inline void MyString::clearString()
+{
+	if (this->str != nullptr)
+	{
+		delete[]this->str;
+	}
+
+	this->str = nullptr;
+}
+
+MyString::~MyString()
+{
+	//очищаем динамическую память выделенную под массив char *str
+	delete []this->str;
+}
+
+inline MyString MyString::operator+(const MyString& obj)
+{
+	MyString tmp;
+
+	tmp.str = new char[strlen(this->str) + strlen(obj.str) + 1];
+
+	//int i = 0;
+
+	//for (; i < strlen(this->str); i++)
+	//{
+	//	tmp.str[i] = this->str[i];
+	//}
+
+	//for (size_t j = 0; j < strlen(obj.str); j++, i++)
+	//{
+	//	tmp.str[i] = obj.str[j];
+	//	
+	//}
+
+	//tmp.str[i] = '\0';
+
+	tmp.str = strcpy(tmp.str, this->str);
+	tmp.str = strcat(tmp.str, obj.str);
+
+	return tmp;
+}
+
+inline MyString &MyString::operator+=(const MyString& obj)
+{
+	MyString tmp;
+
+	tmp.str = new char[strlen(this->str) + strlen(obj.str) + 1];
+	tmp.str = strcpy(tmp.str, this->str);
+	tmp.str = strcat(tmp.str, obj.str);
+
+	this->clearString();
+	
+	this->str = new char[strlen(tmp.str) + 1];
+
+	strcpy(this->str, tmp.str);
+
+	return *this;
+}
+
+inline bool MyString::operator==(const MyString& obj)
+{
+	return !strcmp(this->str, obj.str);
+}
+
+inline bool MyString::operator!=(const MyString& obj)
+{
+	return strcmp(this->str, obj.str);
+}
+
+ostream& operator<<(ostream& out, const MyString& obj)
+{	
+	for (size_t i = 0; i < strlen(obj.str); i++)
+	{
+		out << obj.str[i];
+	}
+
+	out << endl;
+
+	return out;
+}
---
+++ BaseClassString/Main.cpp
@@ -0,0 +1,92 @@
+#include"BaseClassString.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout.setf(std::ios_base::boolalpha);
+
+	locale loc(cout.getloc(), new ::numpunct);
+	cout.imbue(loc);
+
+
+	MyString str("Я строка str");
+
+	//str.print();
+
+	cout << "Cтрока str: " << str;
+
+	int lenght = str.countLenght();
+
+	cout << "Длина строки str: " << lenght << " символа(ов)." << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	MyString str2("Я строка str2");
+
+	cout << "Cтрока str2: " << str2;
+
+	lenght = str2.countLenght();
+
+	cout << "Длина строки str2: " << lenght << " символа(ов)." << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	MyString str3;
+
+	cout << "Cтрока str3 = str2 = str: "; str3 = str2 = str;
+	cout << str3 << endl;
+	lenght = str3.countLenght();
+	cout << "Длина строки str3: " << lenght << " символа(ов)." << endl;
+	cout << "Cтрока str: " << str;
+	cout << "Cтрока str2: " << str2;	
+
+	cout << "------------------------------------------------" << endl;
+
+	str3.clearString();
+	cout << "Cтрока str3.clearString() -> str3(\"Я новая срока str3\")" << endl;
+	str3 = "Я новая срока str3";
+	cout << "Cтрока str3: " << str3;
+	lenght = str3.countLenght();
+	cout << "Длина строки str3: " << lenght << " символа(ов)." << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	MyString str4 = "Я новая срока str4 = str3 + str = ";
+	cout << "Cтрока str4: " << str4;
+
+	str4 = str3 + " + " + str;
+	cout << "Cтрока str4: " << str4 << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	MyString str5 = "Я новая срока str5 += str = ";
+	cout << "Cтрока str5: " << str5;
+
+	str5 += str;
+	cout << "Cтрока str5: " << str5 << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	bool check = str == str2;
+
+	cout << "Cтрока str1 == str2 ? " << check << endl;
+
+	check = str == str3;
+
+	cout << "Cтрока str1 == str3 ? " << check << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	check = str != str2;
+
+	cout << "Cтрока str1 != str2 " << check << endl;
+
+	check = str != str3;
+
+	cout << "Cтрока str1 != str3 " << check << endl;
+
+	cout << endl;
+	system("pause");
+}
